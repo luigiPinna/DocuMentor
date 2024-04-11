cd094c355018d1bc607d783adec6e5896ab4158e
Luigi Pinna <luigipinna3@gmail.com>
2024-04-11 17:05:39 +0200

    set main

--- app/main.py
+++ app/main.py
@@ -3,23 +3,28 @@ from logger import Logger
 from ai_service import AIService
 import sys
 
-# Logger and Setup init
-try:
-    my_setup = Setup()
-    my_logger = Logger('DocuMentorLogger', log_file=my_setup.main_log_file_path).get_logger()
-    my_logger.info("DocuMentor starting...")
-except Exception as e:
-    print(f"Error during initial setup: {e}")
-    sys.exit(1)
 
-# AIService instance
-ai_service = AIService(my_logger, my_setup)
+def main():
+    # Logger and Setup init
+    try:
+        my_setup = Setup()
+        my_logger = Logger('DocuMentorLogger', log_file=my_setup.main_log_file_path).get_logger()
+        my_logger.info("DocuMentor starting...")
+    except Exception as e:
+        print(f"Error during initial setup: {e}")
+        sys.exit(1)
 
-# User question
-question = input("Please enter your question: ")
-my_logger.info(f"Received question: {question}")
+    # AIService instance
+    ai_service = AIService(my_logger, my_setup)
 
-# Query execution
-response = ai_service.perform_query(question)
+    # User question
+    question = input("Please enter your question: ")
+    my_logger.info(f"Received question: {question}")
 
-print(response)
+    # Query execution
+    response = ai_service.perform_query(question)
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
